89d56d4880898cc8bf819b50bfee4b03cc8378b4
bk <bk123yt@gmail.com>
2021-02-25 21:14:13 -0600

    Good progress

--- src/tk/pgfriends/hub/Main.java
+++ src/tk/pgfriends/hub/Main.java
@@ -2,35 +2,31 @@ package tk.pgfriends.hub;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import tk.pgfriends.hub.commands.Hub;
-import tk.pgfriends.hub.events.PlayerEvents;
 
 public class Main extends JavaPlugin {
 	
-	public static HashMap<String, Location> blBr = new HashMap<String, Location>();
+	File file = new File(getDataFolder() + File.separator + "database.yml"); // Creates a File object
+	FileConfiguration database = YamlConfiguration.loadConfiguration(file); // Turns the File object into YAML and loads data
 	
 	@Override
-	public void onEnable()
+	public void onEnable() // When the plugin is enabled
 	{
 		//REGISTRATION for Events and Commands!
-		getServer().getPluginManager().registerEvents(new PlayerEvents(), this); // Registers PlayerEvents events class
+		// (We don't need this right now) -- getServer().getPluginManager().registerEvents(new PlayerEvents(), this); // Registers PlayerEvents events class
         this.getCommand("hub").setExecutor(new Hub()); // Registers Hub command class
         
         
-        
-        
-        // Save data
-		File file = new File(getDataFolder() + File.separator + "database.yml");
-		FileConfiguration database = YamlConfiguration.loadConfiguration(file);
 		
-		if (!file.exists())
+		if (!file.exists()) // If the file doesn't exist, create one
 		{
 			try {
 				file.createNewFile();
@@ -40,7 +36,7 @@ public class Main extends JavaPlugin {
 			}
 		}
 		
-		if (file.exists())
+		if (file.exists()) // If it does exist, load in some data if needed
 		{
 			// ignore for now lol
 			// get the variables from database here
@@ -49,30 +45,47 @@ public class Main extends JavaPlugin {
 		
 		
 		
+		
+		
+		
+		
 	}
 	
 	@Override
-	public void onDisable()
+	public void onDisable() // When the plugin is disabled
 	{
-		
-		File file = new File(getDataFolder() + File.separator + "database.yml");
-		FileConfiguration database = YamlConfiguration.loadConfiguration(file);
-		
-		for (String uuid : blBr.keySet())
-		{
-			database.set("blockBroken.playerData." + uuid, blBr.get(uuid).toString());
-		}
-		
-		
+		// database.set("blockBroken.playerData." + uuid, pLoc.get(uuid).toString());
 		
 		
 		try {
-			database.save(file);
+			database.save(file); // Tries to save file
 
 		} catch (IOException e) {
-			e.printStackTrace();
+			e.printStackTrace(); // Doesn't crash plugin if the above fails
 		}
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void save(Player player, Location loc, Location dest, FileConfiguration db) // Saves the data to the file when called
+	{
+		db.set(loc.getWorld().toString() + ".uuid", loc.serialize());
+		player.teleport(dest); // Teleports sender to the hub if no errors while saving
+	}
+	
+	
+	
+	
+	
+	public static Location load(String uuid, World world, FileConfiguration db) // Loads the data to the plugin when called
+	{
+		return new Location(Location.deserialize(db.get(world.toString() + "." + uuid));
+	}
 }
-
-//nice
--- src/tk/pgfriends/hub/commands/Hub.java
+++ src/tk/pgfriends/hub/commands/Hub.java
@@ -1,36 +1,57 @@
 package tk.pgfriends.hub.commands;
 
+import java.io.File;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
+import tk.pgfriends.hub.Main;
+
 public class Hub implements CommandExecutor { // /hub
+	
+	private Main plugin;
+
+	public Hub(Main plugin) // Helps me from having to use static because File is weird
+	{
+	   this.plugin = plugin;
+	}
+	
+	File file = new File(plugin.getDataFolder() + File.separator + "database.yml"); // Creates a File object
+	FileConfiguration database = YamlConfiguration.loadConfiguration(file); // Turns the File object into YAML and loads data
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (!(sender instanceof Player)) { return true; }
 		
-		
-		if (cmd.equals("hub"))
-		{
-			Player player = (Player) sender;
+			Player player = (Player) sender; // new Player object from CommandSender
+			
+			Location dest = new Location(Bukkit.getWorld("hub"), 0.5, 193, 0.5);
+			
+			
+			
+			if (args.length == 3) { return false; } // Returns false usage of /hub if more than 3 arguments (/hub bkYT str)
 			
 			if (args.length == 2) // If the command looks like: /hub <player>
 			{
-				//TODO send <player> to the hub
+				if (Bukkit.getPlayer(args[1]) == null) { return false; } // If the requested player name doesn't match any online, return false usage
 				
-				return true; // End the event, we are done
+				player = Bukkit.getPlayer(args[1]); // Get the Player object from the requested player name
+				
+				Main.save(player, player.getLocation(), dest, database); // Puts the players UUID and pairs it with their Location in the HashMap in Main
+				
+				return true; // End the event, we are done, kick us out (return true; if successful, return false; if unsucessful)
 			}
 			
-			// If the command is only: /hub
-			// TODO send sender to the hub
-		}
-		
-		
-		
-		
-		return false;
+			
+			Main.save(player, player.getLocation(), dest, database); // Puts the players UUID and pairs it with their Location in the HashMap in Main
+			
+			return true; // You NEED to return a boolean or else it will give error
 	}
 
 
