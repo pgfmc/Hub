3bb005431cc3a03fb92b2b185c871b037ba243d4
bk <bk123yt@gmail.com>
2021-02-27 04:08:31 -0600

    /hub works

--- src/tk/pgfriends/hub/Main.java
+++ src/tk/pgfriends/hub/Main.java
@@ -15,22 +15,18 @@ import tk.pgfriends.hub.commands.Hub;
 
 public class Main extends JavaPlugin {
 	
-	@SuppressWarnings("unused")
-	private Main plugin; // Used for constructor
+	public static Main plugin; // Used for constructor
 	
 	File file = new File(getDataFolder() + File.separator + "database.yml"); // Creates a File object
-	FileConfiguration database = YamlConfiguration.loadConfiguration(file); // Turns the File object into YAML and loads data
-	
-	public Main(Main plugin) // Constructor that lets me create an instance of Main class so I can get plugin without having to use static modifier
-	{
-	   this.plugin = plugin;
-	}
-	
+	FileConfiguration database = YamlConfiguration.loadConfiguration(file); // Turns the File object into YAML and loads data	
 	
 	
 	@Override
 	public void onEnable() // When the plugin is enabled
 	{
+		plugin = this;
+		
+		
 		//REGISTRATION for Events and Commands!
 		// (We don't need this right now) -- getServer().getPluginManager().registerEvents(new PlayerEvents(), this); // Registers PlayerEvents events class
         this.getCommand("hub").setExecutor(new Hub()); // Registers Hub command class
@@ -85,9 +81,17 @@ public class Main extends JavaPlugin {
 	
 	
 	
-	public static void save(Player player, Location loc, Location dest, FileConfiguration db) // Saves the data to the file when called
+	public static void save(Player player, Location loc, Location dest, FileConfiguration db, File file) // Saves the data to the file when called
 	{
-		db.set(loc.getWorld().toString() + ".uuid", loc.serialize());
+		db.set(loc.getWorld().getName() + ".uuid." + player.getUniqueId().toString() + "." + , loc.serialize());
+		
+		try {
+			db.save(file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		player.teleport(dest); // Teleports sender to the hub if no errors while saving
 	}
 	
@@ -99,6 +103,6 @@ public class Main extends JavaPlugin {
 	public static Location load(String uuid, World world, FileConfiguration db) // Loads the data to the plugin when called
 	{
 		
-		return (Location.deserialize((Map<String, Object>) (db.get(world.toString() + "." + uuid))));
+		return (Location.deserialize((Map<String, Object>) (db.get(world.getName() + "." + uuid))));
 	}
 }
--- src/tk/pgfriends/hub/commands/Hub.java
+++ src/tk/pgfriends/hub/commands/Hub.java
@@ -14,16 +14,14 @@ import org.bukkit.entity.Player;
 import tk.pgfriends.hub.Main;
 
 public class Hub implements CommandExecutor { // /hub
-	private Main plugin; // Creates new plugin variable to avoid using static (because File dumb)
-	Main main = new Main(plugin); // Sets the Hub class' private Main plugin to the Main's private Main plugin (getter/setter constructor)
 	
 	// We need to do this again to avoid using static
-	File file = new File(plugin.getDataFolder() + File.separator + "database.yml"); // Creates a File object (plugin.getDataFolder() here now because this class doesn't extend JavaPlugin (not the Main class))
+	File file = new File(Main.plugin.getDataFolder() + File.separator + "database.yml"); // Creates a File object (plugin.getDataFolder() here now because this class doesn't extend JavaPlugin (not the Main class))
 	FileConfiguration database = YamlConfiguration.loadConfiguration(file); // Turns the File object into YAML and loads data
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (!(sender instanceof Player)) { return true; }
+		if (!(sender instanceof Player)) { return false; }
 		
 			Player player = (Player) sender; // new Player object from CommandSender
 			
@@ -39,13 +37,13 @@ public class Hub implements CommandExecutor { // /hub
 				
 				player = Bukkit.getPlayer(args[1]); // Get the Player object from the requested player name
 				
-				Main.save(player, player.getLocation(), dest, database); // Puts the players UUID and pairs it with their Location in the HashMap in Main
+				Main.save(player, player.getLocation(), dest, database, file); // Puts the players UUID and pairs it with their Location in the HashMap in Main
 				
 				return true; // End the event, we are done, kick us out (return true; if successful, return false; if unsucessful)
 			}
 			
 			
-			Main.save(player, player.getLocation(), dest, database); // Puts the players UUID and pairs it with their Location in the HashMap in Main
+			Main.save(player, player.getLocation(), dest, database, file); // Puts the players UUID and pairs it with their Location in the HashMap in Main
 			
 			return true; // You NEED to return a boolean or else it will give error
 	}
